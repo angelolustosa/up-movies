51119cefc9efb4279b4bff0b606995a32abeb00e
Angelo <angelo.filho@cge.ce.gov.br>
2023-07-13 17:38:32 -0300

    ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -84,108 +84,49 @@ function App() {
           </div>
           <button type="submit" className="btn btn-primary">Adicionar Filme</button>
         </form>
-        <div className="row row-cols-1 row-cols-md-6 mt-4">
+        <div className="row">
           {filmes.map((filme, index) => (
-            <div className="col mb-2">
-              <div className="card" /* style={{ height: '44rem' }} */ style={{ height: '38rem' }} >
+            <div class="col-lg-2 mb-3 d-flex align-items-stretch">
+              <div class="card">
                 <img src={`https://image.tmdb.org/t/p/w220_and_h330_face${filme.poster_path}`} className="card-img-top" alt="Filme" />
-                <div className="card-body">
+                <div class="card-body d-flex flex-column">
                   <h5 className="card-title">{filme.title}</h5>
-                  <p>Diretor: {filme.diretor}</p>
-                  <p>Gênero: {filme.genero}</p>
-                  <div id='buttons'>
-                    <button className="btn btn-primary me-2" onClick={() => editarFilme(index)}>
-                      <FaEdit />
-                    </button>
-                    <button className="btn btn-danger" onClick={() => excluirFilme(index)}>
-                      <FaTrash />
-                    </button>
-                  </div>
-                </div>
-                <div className="card-footer">
-                  <small className="text-body-secondary">{`Lançamento: ${moment(filme.release_date).format("DD/MM/YYYY")}`}</small>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-
-        {/* https://codepen.io/Kerrys7777/pen/QWgwEeG?editors=1000*/}
-        {/*   { <div className="col mb-2">
-              <div className="card h-100 ">
-              <img src={`https://image.tmdb.org/t/p/w220_and_h330_face${filme.poster_path}`} className="card-img-top" alt="Filme" />
-                  <div className="card-body">
-                    <h5 className="card-title">{filme.title}</h5>
-                    <p className="card-text">{filme.overview}</p>
-                  </div>
-              </div>
-            </div>} */}
-
-        {/* {} */}
-
-        {/* <div className="container">
-              <div className="col-lg-12 mb-3 d-flex align-items-">
-                <div className="card">
-                  <img src={`https://image.tmdb.org/t/p/w220_and_h330_face${filme.poster_path}`} className="card-img-top" alt="Filme" />
-                  <div className="card-body d-flex flex-column">
-                    <h5 className="card-title">{filme.title}</h5>
-                    <p className="card-text">{filme.overview}</p>
-                    <a href="#" className="btn btn-primary mt-auto align-self-start">Book now</a>
-                  </div>
-                </div>
-              </div>
-            </div> */}
-
-
-        {/* <div className="card-group" style={{width: '20em', height: 'fit-content'}}>
-              {console.log(filme)}
-              <div className="card">
-                <img src={`https://image.tmdb.org/t/p/w220_and_h330_face${filme.poster_path}`} className="card-img-top" alt="Filme" />
-                <div className="card-body">
-                  <h5 className="card-title">{filme.title}</h5>
-                  <p className="card-text">{filme.overview}</p>
-                </div>
-                <div className="card-footer">
-                  <small className="text-body-secondary">{`Lançamento: ${moment(filme.release_date).format("DD/MM/YYYY")}`}</small>
-                </div>
-              </div>
-            </div> */}
-
-        {/* <div className="mt-4">
-          <h4>Filmes Cadastrados:</h4>
-          <ul className="list-group">
-            {filmes.map((filme, index) => (
-              <li key={index} className="list-group-item">{filme.titulo} - {filme.diretor} - {filme.genero}</li>
-            ))}
-          </ul>
-        </div> */}
-      </div>
-
-
-      {/* <div key={index}>
-        {console.log(filme)}
-        <div key={index} className="card" style={{ width: '12em' }}>
-          <img src={`https://image.tmdb.org/t/p/w220_and_h330_face${filme.poster_path}`} className="card-img-top" alt="Filme" />
-          <div className="card-body">
-            <div key={index} className="card" style={{ width: '12em', height: '32rem' }}>
-              <img src={`https://image.tmdb.org/t/p/w220_and_h330_face${filme.poster_path}`} className="card-img-top" alt="Filme" />
-              <div className="card-body">
-                <h5 className="card-title">{filme.title}</h5>
-                <p>Diretor: {filme.diretor}</p>
-                <p>Gênero: {filme.genero}</p>
-                <div className="d-flex justify-content-end">
-                  <button className="btn btn-primary me-2" onClick={() => editarFilme(index)}>
+                  {/* <p class="card-text mb-4">Is a manmade waterway dug in the early 1600's and now displays many landmark commercial locals and vivid neon signs.</p> */}
+                  <p class="card-text mb-4">{filme.overview.trim()}</p>
+                  <a href="#" class="mt-auto align-self-start">
+                    <button className="btn btn-primary" style={{ marginRight: '0.5em'}} onClick={() => editarFilme(index)}>
                     <FaEdit />
                   </button>
                   <button className="btn btn-danger" onClick={() => excluirFilme(index)}>
                     <FaTrash />
                   </button>
-                </div>
+                </a>
+              </div>
+              <div className="card-footer">
+                <small className="text-body-secondary">{`Lançamento: ${moment(filme.release_date).format("DD/MM/YYYY")}`}</small>
               </div>
             </div>
+            </div>
+          ))}
+      </div>
+
+      {/* https://codepen.io/Kerrys7777/pen/QWgwEeG?editors=1000*/}
+      {/*  <div className="row">
+        <div class="col-lg-4 mb-3 d-flex align-items-stretch">
+        <div class="card">
+          <img src={`https://image.tmdb.org/t/p/w220_and_h330_face${filme.poster_path}`} className="card-img-top" alt="Filme" />
+          <div class="card-body d-flex flex-column">
+            <h5 className="card-title">{filme.title}</h5>
+            <p class="card-text mb-4">Is a manmade waterway dug in the early 1600's and now displays many landmark commercial locals and vivid neon signs.</p>
+            <a href="#" class="btn btn-primary mt-auto align-self-start">Book now</a>
           </div>
+          <div className="card-footer">
+                  <small className="text-body-secondary">{`Lançamento: ${moment(filme.release_date).format("DD/MM/YYYY")}`}</small>
+                </div>
         </div>
-      </div> */}
+      </div> 
+      </div>*/}
+    </div >
     </>
   )
 }
